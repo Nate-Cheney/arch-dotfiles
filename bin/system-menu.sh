82ac962b457f06ab4546847d04469cd24f0d84c2
Nate Cheney <natecheney@proton.me>
2025-12-23 22:46:45 -0500

    New system menu

---
+++ bin/system-menu.sh
@@ -0,0 +1,21 @@
+options="App Launcher\nNetwork Manager\nPower Menu\nSound Manager"
+
+num_lines=$(echo -e "$options" | wc -l)
+
+action=$(echo -e "$options" | walker --dmenu -Nn --height "$num_lines")
+
+case $action in
+    "App Launcher")
+        uwsm-app -- walker
+    ;;
+
+    "Network Manager")
+        uwsm-app -- kitty -e impala
+    ;;
+    "Power Menu")
+        uwsm-app -- $HOME/.config/waybar/power-menu.sh
+    ;;
+    "Sound Manager")
+        uwsm-app -- kitty -e wiremix
+    ;;
+esac
