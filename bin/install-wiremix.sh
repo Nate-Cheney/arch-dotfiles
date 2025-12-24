ce51c4196c48aa4e561169c3bcab640376472f78
Nate Cheney <natecheney@proton.me>
2025-12-23 22:14:17 -0500

    Replace PWVU with Wiremix

--- bin/install-wiremix.sh
+++ bin/install-wiremix.sh
@@ -2,12 +2,12 @@
 
 # Author: Nate Cheney
 # Filename: install-wiremix.sh
-# Description: This script installs the wiremix sound manager. 
+# Description: This script installs the wiremix sound manager and the pipewire dependencies. 
 # Usage: sudo ./install-wiremix.sh
 # Options:
 #
 
-package=""
+package="alsa-utils alsa-ucm-conf pipewire pipewire-alsa pipewire-pulse sof-firmware wiremix wireplumber"
 
 if ! pacman -Q $package &> /dev/null; then
     echo "Installing $package..."
@@ -16,3 +16,5 @@ else
     echo "$package is already installed."
 fi
 
+systemctl --user enable wireplumber pipewire pipewire-pulse
+systemctl --user start wireplumber pipewire pipewire-pulse
