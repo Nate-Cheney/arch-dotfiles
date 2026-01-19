a16ec257dd59bc0ae8bbe4cedd86e9e2fa3a4d63
Nate Cheney <natecheney@proton.me>
2026-01-19 14:34:18 -0500

    Teams web app

---
+++ install/chroot/bin/install-teams.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author: Nate Cheney
+# Filename: install-teams.sh 
+# Description: This script creates a Microsoft Teams web application using chromium.
+# Usage: ./install-teams.sh
+# Options:
+#
+
+cat << EOF > /usr/share/applications/teams.desktop 
+[Desktop Entry]
+Name=Microsoft Teams
+Exec=chromium --app=https://teams.cloud.microsoft/
+Icon=/usr/share/icons/Papirus/48x48/apps/teams-for-linux.svg
+Type=Application
+Categories=Network;WebBrowser;
+StartupWMClass=www.cloud.microsoft
+EOF
+
