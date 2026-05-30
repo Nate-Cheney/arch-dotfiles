c52fd822cc6b8e40a2dcb33ede13625ee3060755
Nate Cheney <natecheney@proton.me>
2026-05-30 12:06:41 -0400

    Update animation speeds

--- config/hypr/looknfeel.lua
+++ config/hypr/looknfeel.lua
@@ -1,9 +1,8 @@
 hl.config({
     -- General Configuration
-    -- See https://wiki.hypr.land/Configuring/Variables/#general
     general = {
-        gaps_in = 2,
-        gaps_out = 2,
+        gaps_in = 1,
+        gaps_out = 1,
         border_size = 1,
 
         -- Solid white active border
@@ -15,39 +14,7 @@ hl.config({
         layout = "dwindle"
     },
 
-    -- Animations
-    -- See https://wiki.hypr.land/Configuring/Variables/#animations
-    animations = {
-        enabled = true,
-        bezier = {
-            "easeOutQuint,0.23,1,0.32,1",
-            "easeInOutCubic,0.65,0.05,0.36,1",
-            "linear,0,0,1,1",
-            "almostLinear,0.5,0.5,0.75,1.0",
-            "quick,0.15,0,0.1,1"
-        },
-        animation = {
-            "global, 1, 10, default",
-            "border, 1, 5.39, easeOutQuint",
-            "windows, 1, 4.79, easeOutQuint",
-            "windowsIn, 1, 4.1, easeOutQuint, popin 87%",
-            "windowsOut, 1, 1.49, linear, popin 87%",
-            "fadeIn, 1, 1.73, almostLinear",
-            "fadeOut, 1, 1.46, almostLinear",
-            "fade, 1, 3.03, quick",
-            "layers, 1, 3.81, easeOutQuint",
-            "layersIn, 1, 4, easeOutQuint, fade",
-            "layersOut, 1, 1.5, linear, fade",
-            "fadeLayersIn, 1, 1.79, almostLinear",
-            "fadeLayersOut, 1, 1.39, almostLinear",
-            "workspaces, 1, 1.94, almostLinear, fade",
-            "workspacesIn, 1, 1.21, almostLinear, fade",
-            "workspacesOut, 1, 1.94, almostLinear, fade"
-        }
-    },
-
     -- Decoration
-    -- See https://wiki.hypr.land/Configuring/Variables/#decoration
     decoration = {
         rounding = 0,
         active_opacity = 1.0,
@@ -67,13 +34,11 @@ hl.config({
     },
 
     -- Dwindle Layout
-    -- See https://wiki.hypr.land/Configuring/Dwindle-Layout/
     dwindle = {
         preserve_split = true,
     },
 
     -- Misc
-    -- See https://wiki.hypr.land/Configuring/Variables/#misc
     misc = {
         force_default_wallpaper = 0,
         disable_hyprland_logo = true,
@@ -81,3 +46,27 @@ hl.config({
     }
 })
 
+-- Animations
+hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
+hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
+hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
+hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
+hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
+hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
+hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
+hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, bezier = "almostLinear" })
+hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
+hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
+hl.animation({ leaf = "layersIn", enabled = true, speed = 4, bezier = "easeOutQuint", style = "fade" })
+hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, bezier = "linear", style = "fade" })
+hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
