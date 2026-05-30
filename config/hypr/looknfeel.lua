55d8215d87876834b65abf2e2d4faac4a26c17d3
Nate Cheney <natecheney@proton.me>
2026-05-29 23:36:24 -0400

    Disable splash text

--- config/hypr/looknfeel.lua
+++ config/hypr/looknfeel.lua
@@ -76,7 +76,8 @@ hl.config({
     -- See https://wiki.hypr.land/Configuring/Variables/#misc
     misc = {
         force_default_wallpaper = 0,
-        disable_hyprland_logo = true
+        disable_hyprland_logo = true,
+        disable_splash_rendering = true
     }
 })
 
